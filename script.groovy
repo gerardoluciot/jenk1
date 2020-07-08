6b94d779c6e47aeb3037b065d48ebd25117c87d3
Gerardo Lucio (Contractor) <Gerardo.Lucio@wnco.com>
2020-07-07 20:10:00 -0500

    add groovy file main

--- script.groovy
+++ script.groovy
@@ -1,2 +1,8 @@
 def str = "Hello world"
 def num =0
+
+class Main {                                    
+    static void main(String... args) {          
+        println 'Groovy world!'                 
+    }
+}
